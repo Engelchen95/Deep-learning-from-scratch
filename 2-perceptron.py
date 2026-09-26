99cf638fb6228fda21ff083f69df95bdfc981abb
engelchen95 <sk0903@sjtu.edu.cn>
2026-09-26 08:45:31 +0800

    chapter 2

--- 2-perceptron.py
+++ 2-perceptron.py
@@ -1,4 +1,5 @@
-# %%
+import numpy as np
+# %% 与门的实现
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1*w1 + x2*w2
@@ -6,4 +7,39 @@ def AND(x1, x2):
         return 0
     elif tmp > theta:
         return 1
-# %%
+# %% 使用权重和偏置实现与门/与非门/或门
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+# %% 异或门的实现
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
